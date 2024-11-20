da22ad5218bdc40d5ddd6cf7f4373a4a91f09031
Fabiano_Dantas <fabianorsdantas@gmail.com>
2024-11-20 11:23:15 -0300

    Construindo a página

---
+++ src/component/Page.js
@@ -0,0 +1,11 @@
+import Feed from "./page/Feed";
+import Stories from "./page/Stories";
+
+export default function Page() {
+  return (
+    <main>
+      <Stories />
+      <Feed />
+    </main>
+  );
+}
