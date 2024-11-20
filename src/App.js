f5efe6af22424227aece5792af3a531c9c44e512
Fabiano_Dantas <fabianorsdantas@gmail.com>
2024-11-20 11:37:31 -0300

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+import Page from "./component/Page";
+import Sidebar from "./component/Sidebar";
 
 
 export default function App() {
